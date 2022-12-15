dd967c8f760c730525d96ec2879b9d010f871667
KashbullinRail <kashbullinrail@gmail.com>
2022-12-15 22:12:22 +0300

    add colors in enum COLOR

--- app/src/main/java/com/example/canvas/settings/COLOR.kt
+++ app/src/main/java/com/example/canvas/settings/COLOR.kt
@@ -15,8 +15,11 @@ enum class COLOR(
     PINK(R.color.purple_200),
     TURQUOISE(R.color.teal_200),
     YELLOW(android.R.color.holo_orange_light),
+    PURPLE7(R.color.purple_700),
     L_BLUE(android.R.color.holo_blue_bright),
     ORANGE(android.R.color.holo_orange_dark),
+    GRAY(android.R.color.darker_gray),
+    PURPLE5(R.color.purple_500),
     WHITE(R.color.white);
 
     companion object {
