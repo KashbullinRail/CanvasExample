a38ae28efd996cd2f9e0092b0670f4be7f8d2506
KashbullinRail <kashbullinrail@gmail.com>
2022-12-17 11:47:08 +0300

    create ImageSearchActivity.kt add glide lib, activ viewBinding create getOnRandomImage() fun fill activity_image_search.xml

---
+++ app/src/main/java/com/example/canvas/presentation/ImageSearchActivity.kt
@@ -0,0 +1,96 @@
+package com.example.canvas.presentation
+
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.example.canvas.databinding.ActivityImageSearchBinding
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+
+
+const val URL_IMAGE = "https://source.unsplash.com/random"
+
+class ImageSearchActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityImageSearchBinding
+    private var enable: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityImageSearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnGetRI.setOnClickListener {
+            getOnRandomImage()
+        }
+        binding.etWordForSearch.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                return@setOnEditorActionListener getOnRandomImage()
+            }
+            return@setOnEditorActionListener false
+        }
+        binding.checkBox.setOnClickListener {
+            enableTV()
+        }
+
+    }
+
+    fun getOnRandomImage(): Boolean {
+
+        val keyword = binding.etWordForSearch.text.toString()
+        if (binding.checkBox.isChecked && keyword.isBlank()) {
+            binding.etWordForSearch.error = "Not input text"
+            return true
+        }
+        val inputWord = URLEncoder.encode(keyword, StandardCharsets.UTF_8.name())
+        binding.progressBar.visibility = View.VISIBLE
+        Glide.with(this)
+            .load(
+                URL_IMAGE +
+                        if (binding.checkBox.isChecked) "?$inputWord" else ""
+            )
+            .skipMemoryCache(true)
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: com.bumptech.glide.request.target.Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    binding.progressBar.visibility = View.GONE
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: com.bumptech.glide.request.target.Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    binding.progressBar.visibility = View.GONE
+                    return false
+                }
+            })
+            .into(binding.ivImage)
+        return false
+
+    }
+
+    fun enableTV() = with(binding) {
+        if (checkBox.isChecked) {
+            etWordForSearch.visibility = View.VISIBLE
+        } else {
+            etWordForSearch.visibility = View.GONE
+        }
+    }
+
+}
