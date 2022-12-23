37f5ff9ac9ea7b0c2e700f45c035be892d11dd0b
KashbullinRail <kashbullinrail@gmail.com>
2022-12-23 14:58:57 +0300

    start animation finished timing corrected

--- app/src/main/java/com/example/canvas/presentation/StartActivity.kt
+++ app/src/main/java/com/example/canvas/presentation/StartActivity.kt
@@ -18,7 +18,7 @@ class StartActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 2500)
+        }, 3500)
 
     }
 
